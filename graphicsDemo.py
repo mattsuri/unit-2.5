041c09ff8a2f555f4338086099f18122b7153b44
mattsuri <35492780+mattsuri@users.noreply.github.com>
2018-02-08 12:06:53 -0500

    Updated from Brython Server: 2/8/2018 12:06:53 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -16,12 +16,14 @@ blueCircle = CircleAsset(50, blackOutline, blue) #radius, outline, fill
 greenEllipse = EllipseAsset(100, 50, blackOutline, green) #width, height, outline, fill
 blackLine = LineAsset( 50,160, blackOutline) #xx_endpoint, y_endpoint, lineStyle
 redTriangle = PolygonAsset([(0,0), (120,180), (60,300)], blackOutline, red) #endpoints, outline, fill
+text = TextAsset("Matt", fill=green, style="bold 4- pt Times") #Texxt, other options
 
 Sprite(redRectangle)
 Sprite(blueCircle, (50, 50))
 Sprite(greenEllipse, (200, 400))
 Sprite(blackLine)
 Sprite(redTriangle)
+Sprite(text, (300,200)
 
 App().run() 
 
